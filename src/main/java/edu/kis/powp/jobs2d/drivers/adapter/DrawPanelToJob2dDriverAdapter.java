a9603c31b482bf45c73f8139438a5f39285e4ac7
Kacper Kornacki <60293257+repcus@users.noreply.github.com>
2024-12-08 11:27:35 +0100

    Update and rename MyAdapter.java to DrawPanelToJob2dDriverAdapter.java
    Shape is now inside main window.

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPanelToJob2dDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPanelToJob2dDriverAdapter.java
@@ -4,14 +4,15 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawPanelToJob2dDriverAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public MyAdapter() {
+	public DrawPanelToJob2dDriverAdapter() {
 		super();
 	}
 
@@ -26,8 +27,9 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-
-		drawLine(line);
+		this.startX = x;
+		this.startY = y;
+		DrawerFeature.getDrawerController().drawLine(line);
 	}
 
 	@Override
